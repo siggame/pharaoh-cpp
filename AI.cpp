dab9c2098731b59d12d5932b2734a7f90591a9dc
Adam Harter <athb79@mst.edu>
2015-04-13 13:34:20 -0500

    Progress on ShellAI

--- AI.cpp
+++ AI.cpp
@@ -1,6 +1,9 @@
 #include "AI.h"
 #include "util.h"
 
+#include <cstdlib>
+#include <ctime>
+
 AI::AI(Connection* conn) : BaseAI(conn) {}
 
 const char* AI::username()
@@ -14,12 +17,159 @@ const char* AI::password()
 }
 
 //This function is run once, before your first turn.
-void AI::init(){}
+void AI::init()
+{
+	//set up the random number generator
+	srand(time(NULL));
+	//Find which player I am
+	me = &players[playerID()];
+}
 
 //This function is called each time it is your turn.
 //Return true to end your turn, return false to ask the server for updated information.
 bool AI::run()
 {
+	//if it's time to place traps...
+	if(roundTurnNumber() == 0 || roundTurnNumber() == 1)
+	{
+		//find my sarcophagus
+		for(unsigned i = 0; i < traps.size(); ++i)
+		{
+			Trap& trap = traps[i];
+			if(trap.owner() == playerID() && trap.trapType() == SARCOPHAGUS)
+			{
+				mySarcophagus = &trap;
+				break;
+			}
+		}
+		//find the first open tile
+		for(unsigned i = 0; i < tiles.size(); ++i)
+		{
+			//if the tile is on my side and is empty
+			Tile& tile = tiles[i];
+			if(onMySide(tile.x()) && tile.type() == EMPTY)
+			{
+				//move my sarcophagus to that location
+				me->placeTrap(tile.x(), tile.y(), SARCOPHAGUS);
+			}
+		}
+		//continue spawning traps until there isn't enough money to spend
+		for(unsigned i = 0; i < tiles.size(); ++i)
+		{
+			//if the tile is on my side and is empty
+			Tile& tile = tiles[i];
+			if(onMySide(tile.x()))
+			{
+				//make sure there isn't a trap on that tile
+				if(getTrap(tile.x(), tile.y()) != NULL)
+				{
+					continue;
+				}
+				//select a random trap type (make sure it isn't a sarcophagus)
+				int trapType = (rand() % (trapTypes.size() - 1)) + 1;
+				//if there are enough scarabs
+				if(me->scarabs() >= trapTypes[trapType].cost())
+				{
+					//check if the tile is the right type (wall or empty)
+					if(trapTypes[trapType].canPlaceOnWalls() && tile.type() == WALL)
+					{
+						me->placeTrap(tile.x(), tile.y(), trapType);
+					}
+					else if(!trapTypes[trapType].canPlaceOnWalls() && tile.type() == EMPTY)
+					{
+						me->placeTrap(tile.x(), tile.y(), trapType);
+					}
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+	//otherwise it's time to move and purchase thiefs
+	else
+	{
+		//find my sarcophagus or the enemy sarcophagus
+		for(unsigned i = 0; i < traps.size(); ++i)
+		{
+			Trap& trap = traps[i];
+			if(trap.trapType() == 0)
+			{
+				if(trap.owner() != playerID())
+				{
+					enemySarcophagus = &trap;
+				}
+				else
+				{
+					mySarcophagus = &trap;
+				}
+			}
+		}
+		//find my spawn tiles
+		std::vector<Tile*> spawnTiles = getMySpawns();
+		//select a random thief type
+		int thiefNo = 1;//rand() % thiefTypes.size();
+		if(me->scarabs() >= thiefTypes[thiefNo].cost())
+		{
+			//select a random spawn location
+			int spawnLoc = rand() % spawnTiles.size();
+			//spawn a thief there
+			Tile* spawnTile = spawnTiles[spawnLoc];
+			me->purchaseThief(spawnTile->x(), spawnTile->y(), thiefNo);
+		}
+		//move my thieves
+		std::vector<Thief*> myThieves = getMyThieves();
+		for(unsigned i = 0; i < myThieves.size(); ++i)
+		{
+			Thief& thief = *myThieves[i];
+			//if the thief is alive and not frozen
+			if(thief.alive() && thief.frozenTurnsLeft() == 0)
+			{
+
+				const int xChange[] = {-1, 1,  0, 0};
+				const int yChange[] = { 0, 0, -1, 1};
+				//try to dig or use a bomb before moving
+				if(thief.thiefType() == DIGGER && thief.specialsLeft() > 0)
+				{
+					for(unsigned i = 0; i < 4; ++i)
+					{
+						//if there is a wall adjacent and an empty space on the other side
+						int checkX = thief.x() + xChange[i];
+						int checkY = thief.y() + yChange[i];
+						Tile* wallTile = getTile(checkX, checkY);
+						Tile* emptyTile = getTile(checkX + xChange[i], checkY + yChange[i]);
+						//must be on the map, and not trying to dig to the other side
+						if(wallTile != NULL && emptyTile != NULL && !onMySide(checkX + xChange[i]))
+						{
+							//if the there is a wall with an empty tile on the other side
+							if(wallTile->type() == WALL && emptyTile->type() == EMPTY)
+							{
+								//dig through the wall
+								thief.act(checkX, checkY);
+								//break out of the loop
+								break;
+							}
+						}
+					}
+				}
+				//if the thief has any movement left
+				if(thief.movementLeft() > 0)
+				{
+					//find a path from the thief's location to the enemy sarcophagus
+					std::deque<Point> path;
+					int endX = enemySarcophagus->x();
+					int endY = enemySarcophagus->y();
+					path = findPath(Point(thief.x(), thief.y()), Point(endX, endY));
+					//if a path exists then move forward on the path
+					if(path.size() > 0)
+					{
+						thief.move(path[0].x, path[0].y);
+					}	
+				}
+			}
+		}
+	}
   return true;
 }
 
--- AI.h
+++ AI.h
@@ -2,10 +2,177 @@
 #define AI_H
 
 #include "BaseAI.h"
+#include <deque>
+#include <map>
+
+struct Point
+{
+  int x;
+  int y;
+  Point(){}
+  Point(int x, int y):x(x),y(y){}
+};
 
 ///The class implementing gameplay logic.
 class AI: public BaseAI
 {
+	enum
+	{
+		EMPTY = 0,
+		SPAWN,
+		WALL,
+	};
+	enum
+	{
+		BOMBER = 0,
+		DIGGER,
+		NINJA,
+		GUIDE,
+		SLAVE,
+	};
+	enum
+	{
+		SARCOPHAGUS = 0,
+		SNAKE_PIT,
+		SWINGING_BLADE,
+		BOULDER,
+		SPIDER_WEB,
+		QUICKSAND,
+		OIL_VASES,
+		ARROW_WALL,
+		HEAD_WIRE,
+		MERCURY_PIT,
+		MUMMY,
+		FAKE_ROTATING_WALL,
+	};
+	//returns true if the position is on your side of the field
+	bool onMySide(int x)
+	{
+		if(playerID() == 0)
+		{
+			return (x < mapWidth()/2);
+		}
+		else
+		{
+			return (x >= mapWidth()/2);
+		}
+	}
+
+	//returns the tile at the given x,y position or NULL otherwise
+	Tile* getTile(int x, int y)
+	{
+		if(x < 0 || x >= mapWidth() || y < 0 || y >= mapHeight())
+		{
+			return NULL;
+		}
+		return &tiles[y + x * mapHeight()];
+	}
+
+	//returns the trap at the given x,y position or NULL otherwise
+	Trap* getTrap(int x, int y)
+	{
+		if(x < 0 || x>= mapWidth() || y < 0 || y >= mapHeight())
+		{
+			return NULL;
+		}
+		for(int i = 0; i < traps.size(); ++i)
+		{
+			if(traps[i].x() == x && traps[i].y() == y)
+			{
+				return &traps[i];
+			}
+		}
+		return NULL;
+	}
+
+	//returns a vector of all of your spawn tiles
+	std::vector<Tile*> getMySpawns()
+	{
+		std::vector<Tile*> toReturn;
+		for(unsigned i = 0; i < tiles.size(); ++i)
+		{
+			if(!onMySide(tiles[i].x()) && tiles[i].type() == SPAWN)
+			{
+				toReturn.push_back(&tiles[i]);
+			}
+		}
+		return toReturn;
+	}
+
+	//return a vector of all of your theives
+	std::vector<Thief*> getMyThieves()
+	{
+		std::vector<Thief*> toReturn;
+		for(unsigned i = 0; i < thiefs.size(); ++i)
+		{
+			if(thiefs[i].owner() == playerID())
+			{
+				toReturn.push_back(&thiefs[i]);
+			}
+		}
+		return toReturn;
+	}
+
+	//return a vector of all of the enemy thieves
+	std::vector<Thief*> getEnemyThieves()
+	{
+		std::vector<Thief*> toReturn;
+		for(unsigned i = 0; i < thiefs.size(); ++i)
+		{
+			if(thiefs[i].owner() != playerID())
+			{
+				toReturn.push_back(&thiefs[i]);
+			}
+		}
+		return toReturn;
+	}
+
+  //returns a path from start to end, or nothing if no path is found.
+  std::deque<Point> findPath(Point start, Point end)
+  {
+  	std::deque<Point> toReturn;
+
+	  std::deque<Tile*> theseTiles;
+	  //points back to parent tile
+ 	  std::map<Tile*, Tile*> parent;
+	  theseTiles.push_back(getTile(start.x, start.y));
+	  parent[getTile(start.x, start.y)] = NULL;
+	  Tile* endTile = getTile(end.x, end.y);
+	  while(parent.count(endTile) == 0)
+	  {
+	  	if(theseTiles.empty())
+	  	{
+	  		return toReturn;
+	  	}
+	  	Tile* curTile = theseTiles.front();
+	  	theseTiles.pop_front();
+			const int xChange[]{ 0, 0, -1, 1};
+			const int yChange[]{-1, 1,  0, 0};
+			for(unsigned i = 0; i < 4; ++i)
+			{
+				Point loc(curTile->x() + xChange[i], curTile->y() + yChange[i]);
+				Tile* toAdd = getTile(loc.x, loc.y);
+				if(toAdd != NULL)
+				{
+					if(toAdd->type() == EMPTY && parent.count(toAdd) == 0)
+					{
+						theseTiles.push_back(toAdd);
+						parent[toAdd] = curTile;
+					}
+				}
+			}
+	  }
+	  for(Tile* tile = getTile(end.x, end.y); parent[tile] != NULL; tile = parent[tile])
+	  {
+	  	toReturn.push_front(Point(tile->x(), tile->y()));
+	  }
+	  return toReturn;
+  }
+
+	Player* me;
+	Trap* mySarcophagus;
+	Trap* enemySarcophagus;
+
 public:
   AI(Connection* c);
   virtual const char* username();
